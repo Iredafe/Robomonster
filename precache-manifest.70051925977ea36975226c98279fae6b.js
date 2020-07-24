b17f145b76887c2ed3d86101e14e4c604ba83248
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-07-23 18:01:32 -0700

    Updates

---
+++ precache-manifest.70051925977ea36975226c98279fae6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9cf2f53f77695e21d2443858960f1d",
+    "url": "/Iredafe/Robomonster/index.html"
+  },
+  {
+    "revision": "c82b9378bb36f255288c",
+    "url": "/Iredafe/Robomonster/static/css/main.ed1d830e.chunk.css"
+  },
+  {
+    "revision": "002da3bea13d14657abc",
+    "url": "/Iredafe/Robomonster/static/js/2.991ffe15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Iredafe/Robomonster/static/js/2.991ffe15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82b9378bb36f255288c",
+    "url": "/Iredafe/Robomonster/static/js/main.95e8580c.chunk.js"
+  },
+  {
+    "revision": "5632240d23f7a39fa7bf",
+    "url": "/Iredafe/Robomonster/static/js/runtime-main.13e242bc.js"
+  }
+]);
